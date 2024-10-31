d39b6b61fa61ee1783f84bd303b4c1b564257c80
Abstract Programmer <AbstractProgrammer@example.com>
2024-10-31 14:41:33 +0100

    new

--- article.php
+++ article.php
@@ -1,4 +1,5 @@
 <?php
+
 include "dbconnection.php";
 
 $response = [];
@@ -6,18 +7,10 @@ $response = [];
 $select_user = $conn->prepare("SELECT * FROM blogs");
 $select_user->execute();
 
-if ($select_user->rowCount() > 0) {
+if($select_user->rowCount() > 0){
     $fetch_user = $select_user->fetch(PDO::FETCH_ASSOC);
-    
-    // Encode the image_01 data to base64 if it exists
-    if (!empty($fetch_user['image_01'])) {
-        $fetch_user['image_01'] = base64_encode($fetch_user['image_01']);
-    }
-    
     $response["data"][] = $fetch_user;
     $response["message"] = "login successful";
-} else {
-    $response["message"] = "no data found";
 }
 
 echo json_encode($response);

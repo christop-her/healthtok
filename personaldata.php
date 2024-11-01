a671004c15bf6a6d7fb321b4b983e1c514c5a2f9
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-01 10:31:34 +0100

    nw

---
+++ personaldata.php
@@ -0,0 +1,29 @@
+<?php
+include 'dbconnection.php';
+$response = [];
+$email = $_POST['email'];
+$newemail = $_POST['newemail'];
+$username = $_POST['username'];
+$gender = $_POST['gender'];
+$phone = $_POST['phone'];
+$dob = $_POST['dateofbirth'];
+
+
+// Prepare the SQL statement
+$update_profile = $conn->prepare("UPDATE practitioner SET email = ?, username = ?, gender = ?, phone = ?, dateofbirth = ? WHERE email = ?");
+
+// Execute the SQL statement
+if ($update_profile->execute([$newemail, $username, $gender, $phone, $dateofbirth, $email])) {
+    // Check if any row was affected
+    if ($update_profile->rowCount() > 0) {
+        $response["success"] = "update successful";
+    } else {
+        $response["error"] = "No store found with the given email.";
+    }
+} else {
+    $response["error"] = "Failed to update store profile.";
+}
+
+// Output the JSON response
+echo json_encode($response);
+?>

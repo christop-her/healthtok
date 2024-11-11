4be271fd8b2f0629184a3cae92e9900b9299f360
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-11 14:13:59 +0100

    new

--- department.php
+++ department.php
@@ -4,13 +4,14 @@ include "dbconnection.php";
 
 $response = [];
 $department = $_POST['department'];
-$select_user = $conn->prepare("SELECT * FROM practitioner Where department = ?");
+$select_user = $conn->prepare("SELECT * FROM practitioner WHERE department = ?");
 $select_user->execute([$department]);
 
-if($select_user->rowCount() > 0){
-    $fetch_user = $select_user->fetch(PDO::FETCH_ASSOC);
-    $response["data"][] = $fetch_user;
+if ($select_user->rowCount() > 0) {
+    $response["data"] = $select_user->fetchAll(PDO::FETCH_ASSOC);
     $response["message"] = "login successful";
+} else {
+    $response["message"] = "No practitioners found";
 }
 
 echo json_encode($response);

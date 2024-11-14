0e25d0e2855f5faf84ee41d72fbcab26505a58f4
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-14 11:36:06 +0100

    new

---
+++ MessageLastId2.php
@@ -0,0 +1,21 @@
+<?php
+
+include "dbconnection.php";
+
+$DoctorEmail = $_POST['doctoremail'];
+$email = $_POST['email'];
+$status = false; 
+
+$response = [];
+
+$select_data = $conn->prepare("SELECT * FROM chats WHERE email = ? AND doctoremail = ? AND read_status = ?");
+$select_data->execute([$email, $DoctorEmail, $status]);
+
+if($select_data->rowCount() > 0){
+    while($fetch_data = $select_data->fetch(PDO::FETCH_ASSOC)){ 
+    $response[] = $fetch_data;
+    $response["message"] = "login successful";
+    }
+}
+
+echo json_encode($response);

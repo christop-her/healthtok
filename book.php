512f0f5f33d2c8b33433dc10b7aff2ac08188e23
Abstract Programmer <AbstractProgrammer@example.com>
2024-12-03 14:52:15 +0100

    new

--- book.php
+++ book.php
@@ -20,11 +20,19 @@ if (strpos($contentType, 'application/json') !== false) {
    $DoctorEmail = $data['DoctorEmail']; 
    $email =  $data['email'];
    $reason =  $data['reason'];
+   $transaction_id =  $data['transaction_id'];
+   $txRef =  $data['txRef'];
+   $date =  $data['Adate'];
+   $time =  $data['Atime'];
    $A_status = "unconfirmed";
 } elseif (strpos($contentType, 'multipart/form-data') !== false || strpos($contentType, 'application/x-www-form-urlencoded') !== false) {
    $DoctorEmail = $_POST['DoctorEmail'];
    $email = $_POST['email'];
    $reason = $_POST['reason'];
+   $transaction_id =  $_POST['transaction_id'];
+   $txRef =  $_POST['txRef'];
+   $date =  $_POST['Adate'];
+   $time =  $_POST['Atime'];
    $A_status = "unconfirmed";
 } else {
     $response['message'] = 'Unsupported content type: ' . $contentType;
@@ -33,8 +41,8 @@ if (strpos($contentType, 'application/json') !== false) {
 }
 
     
-   $select_user = $conn->prepare("SELECT * FROM bookings WHERE email = ? AND DoctorEmail = ? AND reason = ?");
-   $select_user->execute([$email, $DoctorEmail, $reason,]);
+   $select_user = $conn->prepare("SELECT * FROM bookings WHERE email = ? AND DoctorEmail = ? AND reason = ? AND Adate = ? AND Atime = ?");
+   $select_user->execute([$email, $DoctorEmail, $reason, $date, $time]);
    $row = $select_user->fetch(PDO::FETCH_ASSOC);
 
    if($select_user->rowCount() > 0){
@@ -44,8 +52,8 @@ if (strpos($contentType, 'application/json') !== false) {
        exit();
 
     }else{
-   $insert_user = $conn->prepare("INSERT INTO bookings(DoctorEmail, email, reason, A_status, created_at) VALUES(?,?,?,?,CURRENT_DATE)");
-   $insert_user->execute([$DoctorEmail, $email, $reason, $A_status]);
+   $insert_user = $conn->prepare("INSERT INTO bookings(DoctorEmail, email, reason, A_status, transaction_id, txRef, Adate, Atime, created_at) VALUES(?,?,?,?,?,?,?,?,CURRENT_DATE)");
+   $insert_user->execute([$DoctorEmail, $email, $reason, $A_status, $transaction_id, $txRef, $date, $time]);
    
    $response["message"] = "booked successfully";
     }

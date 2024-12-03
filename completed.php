da20369db5d5c0043e606cf52abcfdc7b03f3577
Abstract Programmer <AbstractProgrammer@example.com>
2024-12-03 22:27:37 +0100

    new

--- completed.php
+++ completed.php
@@ -4,12 +4,14 @@ include "dbconnection.php";
 try {
     $DoctorEmail = $_POST['DoctorEmail'];
     $email = $_POST['email'];
+    $time = $_POST['time'];
+    $day = $_POST['day'];
     $true = 'true';
     $false = 'false';
 
     // Prepare and execute the update query
-    $update_status = $conn->prepare("UPDATE bookings SET read_status = ? WHERE email = ? AND DoctorEmail = ? AND read_status = ?");
-    $update_status->execute([$true, $email, $DoctorEmail, $false]);
+    $update_status = $conn->prepare("UPDATE bookings SET read_status = ? WHERE email = ? AND DoctorEmail = ? AND Atime = ? AND Adate = ? AND read_status = ?");
+    $update_status->execute([$true, $email, $DoctorEmail, $time, $day, $false]);
 
     // Check if rows were affected
     if ($update_status->rowCount() > 0) {

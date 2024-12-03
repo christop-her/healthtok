d22495fa9a9a19352b879665e6ec414dcf1bdb8b
Abstract Programmer <AbstractProgrammer@example.com>
2024-12-03 23:09:08 +0100

    new

--- completed.php
+++ completed.php
@@ -10,11 +10,16 @@ try {
     $false = 'false';
 
     // Prepare and execute the update query
-    $update_status = $conn->prepare("UPDATE bookings SET read_status = ? WHERE email = ? AND DoctorEmail = ? AND atime = ? AND adate = ? AND read_status = ?");
-    $update_status->execute([$true, $email, $DoctorEmail, $time, $day, $false]);
+    // $update_status = $conn->prepare("UPDATE bookings SET read_status = ? WHERE email = ? AND DoctorEmail = ? AND Atime = ? AND Adate = ? AND read_status = ?");
+    // $update_status->execute([$true, $email, $DoctorEmail, $time, $day, $false]);
+
+    $query = "UPDATE bookings SET read_status = 'true' WHERE email = ? AND DoctorEmail = ? AND Atime = ? AND Adate = ? AND read_status = 'false'";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ssss", $email, $doctorEmail, $time, $day);
+$stmt->execute();
 
     // Check if rows were affected
-    if ($update_status->rowCount() > 0) {
+    if ($query->rowCount() > 0) {
         $response["message"] = "Status updated successfully";
     } else {
         $response["message"] = "No records to update.";

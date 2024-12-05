cc66da214064ba28429a8c32fa6778aef1f4600c
Abstract Programmer <AbstractProgrammer@example.com>
2024-12-05 15:14:28 +0100

    new

--- injectable.php
+++ injectable.php
@@ -2,18 +2,19 @@
 include "dbconnection.php";
 
     $injectable = $_POST['injectable'];
+    $oral = $_POST['oral'];
     $email = $_POST['email'];
 
 
     $response = [];
     
-    $select_data = $conn->prepare("SELECT * FROM injectables WHERE injectable = ? AND email = ?");
-    $select_data->execute([$injectable, $email]);
+    $select_data = $conn->prepare("SELECT * FROM prescription WHERE injectable = ? AND oral = ? AND email = ?");
+    $select_data->execute([$injectable, $oral, $email]);
 
     if($select_data->rowCount() == 0){
 
-        $insert_data = $conn->prepare("INSERT INTO injectables(injectable, email, created_at) VALUES(?,?,CURRENT_DATE)");
-        $insert_data->execute([$injectable, $email]);
+        $insert_data = $conn->prepare("INSERT INTO prescription(injectable, oral, email, created_at) VALUES(?,?,?,CURRENT_DATE)");
+        $insert_data->execute([$injectable, $oral, $email]);
     
         $response["message"] = "successful";
 }
